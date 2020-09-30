70ac27be1ee9dd34b775dd780973ebe4865dba8c
unsxf <2721011139@qq.com>
2020-09-30 10:37:33 +0800

    课程类别实体类

--- src/main/java/com/goodstudy/domain/Kind.java
+++ src/main/java/com/goodstudy/domain/Kind.java
@@ -10,14 +10,39 @@ public class Kind {
     }
     //有参构造
 
-
+    public Kind(int kindId, String kindName) {
+        this.kindId = kindId;
+        this.kindName = kindName;
+    }
 
 
     //toString方法
 
+    @Override
+    public String toString() {
+        return "Kind{" +
+                "kindId=" + kindId +
+                ", kindName='" + kindName + '\'' +
+                '}';
+    }
 
 
     //get和set方法
 
 
+    public int getKindId() {
+        return kindId;
+    }
+
+    public void setKindId(int kindId) {
+        this.kindId = kindId;
+    }
+
+    public String getKindName() {
+        return kindName;
+    }
+
+    public void setKindName(String kindName) {
+        this.kindName = kindName;
+    }
 }
